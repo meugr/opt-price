c86023b0c142189788bc65a647b5bb1e25362501
meugr <meugr32@gmail.com>
2019-03-05 18:02:34 +0300

    Update main.py
    Replaced f with .format, to support python 3.5
    Add exception text to email message
    Minor fixes

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python3
 from config import Config
 import requests
 import json
 import os
 import openpyxl
-from datetime import datetime
+from datetime import datetime as dt
 from shutil import copyfile
 import smtplib
 try:
@@ -12,23 +13,23 @@ except ModuleNotFoundError:
     import xml.etree.ElementTree as etree
 
 
-def get_xml_file(xml_link, path_xml):
+def get_xml_file(xml_link, xml_name):
     """
     Get and write XML from URL.
     """
     xml_price = requests.get(xml_link).content
-    with open(path_xml, "wb") as f:
+    with open(xml_name, "wb") as f:
         f.write(xml_price)
 
 
-def make_dict(path_xml, path_urls):
+def make_dict(xml_name, urls_name):
     """
     Сreates a dictionary from an XML file accessible by xml_name.
     """
     dict_values = {}
-    with open(path_urls, "r") as f:
+    with open(urls_name, "r") as f:
         urls = json.load(f)  # urls = {art1: url1, art2: url2, ...}
-    tree = etree.parse(path_xml)
+    tree = etree.parse(xml_name)
     root = tree.getroot()
     categories = root[0][4]
     offers = root[0][5]
@@ -40,7 +41,7 @@ def make_dict(path_xml, path_urls):
         weight = offer.find('param[@name="Вес"]').text
 
         category_id = offer.find('categoryId').text
-        category = categories.find(f'category[@id="{category_id}"]').text
+        category = categories.find('category[@id="{}"]'.format(category_id)).text
 
         vendor = offer.find('vendor')  # maybe None
         vendor = vendor.text if vendor is not None else 'Без бренда'
@@ -61,34 +62,34 @@ def make_dict(path_xml, path_urls):
     return dict_values
 
 
-def write_in_excel(dict_price, path_excel):
+def write_in_excel(dict_price, excel_name, current_date):
     """
     Writes values from dictionary to an Excel price list.
     Clear 50 rows after last row.
     """
     row = 7  # starting row for write
-    wb = openpyxl.load_workbook(path_excel)
+    wb = openpyxl.load_workbook(excel_name)
     ws = wb.active
+    current_date = dt.now().strftime("%d.%m.%Y")
 
     for art in dict_price:
         row += 1
-        ws[f'A{row}'] = art
-        ws[f'B{row}'] = dict_price.get(art).get('name')
-        ws[f'C{row}'] = dict_price.get(art).get('category')
-        ws[f'D{row}'] = dict_price.get(art).get('vendor')
-        ws[f'E{row}'] = dict_price.get(art).get('flavor')
-        ws[f'F{row}'] = dict_price.get(art).get('weight')
-        ws[f'G{row}'] = dict_price.get(art).get('price')
-        ws[f'H{row}'].hyperlink = dict_price.get(art).get('url')
-    ws['A1'] = f'Цены и наличие\nактуальны на\n\
-{datetime.now().strftime("%d.%m.%Y")}'
+        ws['A{}'.format(row)] = art
+        ws['B{}'.format(row)] = dict_price.get(art).get('name')
+        ws['C{}'.format(row)] = dict_price.get(art).get('category')
+        ws['D{}'.format(row)] = dict_price.get(art).get('vendor')
+        ws['E{}'.format(row)] = dict_price.get(art).get('flavor')
+        ws['F{}'.format(row)] = dict_price.get(art).get('weight')
+        ws['G{}'.format(row)] = dict_price.get(art).get('price')
+        ws['H{}'.format(row)].hyperlink = dict_price.get(art).get('url')
+    ws['A1'] = 'Цены и наличие\nактуальны на\n{}'.format(current_date)
     for i in range(row + 1, row + 50):  # clear 50 row after last row
         for j in range(1, 9):
             ws.cell(row=i, column=j, value='')
-    wb.save(path_excel)
+    wb.save(excel_name)
 
 
-def send_error_message(email_logpass_sender, addressee):
+def send_error_message(email_logpass_sender, addressee, e):
     """
     Send e-mail with error message.
     email_logpass_sender = (login, password)
@@ -98,7 +99,7 @@ def send_error_message(email_logpass_sender, addressee):
     TO = addressee
     FROM = email_logpass_sender[0]
     text = 'Error generating xlsx, urgently check the operation \
-of the script "opt-price".'
+of the script "opt-price".\n\n{}'.format(e)
 
     BODY = "\r\n".join((
            "From: %s" % FROM,
@@ -114,14 +115,17 @@ of the script "opt-price".'
 
 try:
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    get_xml_file(Config.xml_link, Config.path_xml)
-    dict_price = make_dict(Config.path_xml, Config.path_urls)
-    write_in_excel(dict_price, Config.path_excel)
+    current_date = dt.now().strftime("%d.%m.%Y")
+
+    get_xml_file(Config.xml_link, Config.xml_name)
+    dict_price = make_dict(Config.xml_name, Config.urls_name)
+    write_in_excel(dict_price, Config.excel_name, current_date)
     try:
         os.mkdir('backup')  # creates a directory when you first start
     except FileExistsError:
-        print('Directory "backup" exist')
-    copyfile(Config.path_excel, Config.backup_path)
-except:
-    print('ERROR!!')
-    send_error_message(Config.email_logpass_sender, Config.addressee)
+        print('Directory "backup" already exist')
+    backup_path = os.path.join('backup', '{}.xls'.format(current_date))
+    copyfile(Config.excel_name, backup_path)
+except BaseException as e:
+    print('ERROR!!\n', e)
+    send_error_message(Config.email_logpass_sender, Config.addressee, e)
